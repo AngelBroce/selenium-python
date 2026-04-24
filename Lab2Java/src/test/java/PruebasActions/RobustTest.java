a027e47dbc3e53b1d42b5f82f81f54ef0ab99cba
Ccrashh <SMoralesF1403@gmail.com>
2026-04-23 20:17:39 -0500

    pruebas 15, momento java

---
+++ Lab2Java/src/test/java/PruebasActions/RobustTest.java
@@ -0,0 +1,60 @@
+package PruebasActions;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RobustTest {
+
+    private static ExtentReports extent;
+    private ExtentTest test;
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupReport() {
+        // Inicializa el reportero Spark
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+    }
+
+    @BeforeEach
+    public void setup(TestInfo testInfo) {
+        // Crea una entrada para cada prueba individual
+        test = extent.createTest(testInfo.getDisplayName());
+
+        // ... inicialización de tu driver (ChromeOptions, etc)
+        test.info("Navegador inicializado");
+    }
+
+    @Test
+    public void testRobustFlujoCompleto() {
+        test.info("Iniciando flujo de login...");
+        // ... tu lógica de prueba ...
+        test.pass("Login exitoso y producto agregado al carrito");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @AfterAll
+    public static void tearDownReport() {
+        // Obligatorio para generar el archivo final
+        extent.flush();
+    }
+}
--- Lab2Java/src/test/java/PruebasActions/pruebaJava.java
+++
@@ -1,4 +0,0 @@
-package PruebasActions;
-
-public class pruebaJava {
-}
